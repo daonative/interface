74d2ceb8632bf377babd96027a1bae38616b65ca
Benjamin Shafii <ben@prologe.io>
2021-09-14 16:01:04 +0200

    fix(Card): huge shadow overflow bug

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,9 @@
 export const Card = ({ children, className }) => {
   return (
     <div
-      className={`w-full flex flex-col relative bg-white ${className} shadow rounded-md`}
+      className={`w-full flex flex-col relative bg-white ${className} shadow rounded-md overflow-hidden`}
     >
       {children}
     </div>
   );
 };
-

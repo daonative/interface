31739090559f94ec59b9ebc323d8de8cdfce9cd9
Benjamin Shafii <ben@prologe.io>
2021-09-20 16:10:07 +0200

    feat(Logo): redirect should be customizable

--- components/LandingNav.js
+++ components/LandingNav.js
@@ -1,9 +1,12 @@
 import { AnimatedLogo } from "./AnimatedLogo";
+import Link from "next/link";
 
 export const LandingNav = () => (
   <nav>
-    <div className="flex justify-between items-center">
-      <AnimatedLogo />
-    </div>
+    <Link href="/app">
+      <div className="flex justify-between items-center">
+        <AnimatedLogo />
+      </div>
+    </Link>
   </nav>
 );
--- components/Logo.js
+++ components/Logo.js
@@ -5,19 +5,17 @@ export const BLUE = "#3200F5";
 export const StyledNav = styled.div`
   font-size: clamp(1.3rem, 2.1vw, 2.8rem);
   cursor: pointer;
-  font-family: 'Space Grotesk';
+  font-family: "Space Grotesk";
 `;
 
 export const Logo = () => {
   return (
-    <Link href="/">
-      <StyledNav>
-        <span className="font-semibold">prologe</span>
-        <span className="font-bold" style={{ color: BLUE }}>
-          .
-        </span>
-        <span className="font-light">press</span>
-      </StyledNav>
-    </Link>
+    <StyledNav>
+      <span className="font-semibold">prologe</span>
+      <span className="font-bold" style={{ color: BLUE }}>
+        .
+      </span>
+      <span className="font-light">press</span>
+    </StyledNav>
   );
 };

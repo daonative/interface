422d8d72a35f8d45f7c81a7b4dd585676ebf87be
Benjamin Shafii <ben@prologe.io>
2021-09-23 08:39:02 +0200

    style(CallBody): use tailwined prose instead custom styles

--- components/StyledBody.js
+++ components/StyledBody.js
@@ -1,18 +1,9 @@
 import styled from "styled-components";
 
-const StyledBody = styled.div`
-  li:before {
-    content: "•";
-    margin-right: 0.2rem;
-  }
-  li {
-    display: flex;
-  }
-`;
 
 export const CallBody = ({ description }) => {
   return (
-    <StyledBody
+    <div
       className="prose"
       dangerouslySetInnerHTML={{
         __html: description,
